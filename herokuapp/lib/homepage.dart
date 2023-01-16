286f9ec31a34faa3c0ff17d81a77707241dc874e
dagmawi77 <dagmawiletarik@gmail.com>
2023-01-16 11:24:28 +0300

    herokuapp V!

--- herokuapp/lib/homepage.dart
+++ herokuapp/lib/homepage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+//url: 'https://magic-1.herokuapp.com/login';
 class HomePage extends StatefulWidget {
   HomePage({Key? key}) : super(key: key);
 
@@ -9,6 +11,42 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return WebviewScaffold(
+      url: 'https://magic-1.herokuapp.com/login',
+      appBar: AppBar(
+        title: Text("herokuapp"),
+        centerTitle: true,
+        elevation: 0,
+      ),
+        withZoom: true,
+      withLocalStorage: true,
+      scrollBar: true,
+      withJavascript: true,
+      // initialChild: Center(child: Text('Loading...')),
+    
+      // bottomNavigationBar: Padding(
+      //   padding: EdgeInsets.all(12),
+      //   child: Text('This is the bottomNavigationBar on a webview page.'),
+      // ),
+      // persistentFooterButtons: [
+      //   CircleAvatar(
+      //     backgroundColor: Colors.purple,
+      //     child: Text('btn1'),
+      //   ),
+      //   CircleAvatar(
+      //     backgroundColor: Colors.orange,
+      //     child: Text('btn2'),
+      //   ),
+      //   CircleAvatar(
+      //     backgroundColor: Colors.red,
+      //     child: Text('btn3'),
+      //   ),
+      //   CircleAvatar(
+      //     backgroundColor: Colors.grey[700],
+      //     child: Text('btn4'),
+      //   ),
+      // ], 
+       
+    );
   }
 }
